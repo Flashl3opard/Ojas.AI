3d11c92e943c9285b24c3cfcd10c3f3684a03218
FlashL3opard <69573060+Flashl3opard@users.noreply.github.com>
2025-09-12 20:39:31 +0530

    Changes in Navlinks

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,8 +7,9 @@ export default function Navbar() {
     "bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition";
 
   return (
-    <nav className="flex items-center justify-between py-4 px-16 shadow-sm bg-white">
+    <nav className="flex items-center justify-between py-4 px-6 md:px-16 shadow-sm bg-white">
       <Logo />
+
       {/* Navigation links, hidden on small screens */}
       <ul className="hidden md:flex space-x-8 text-gray-700 font-medium">
         <li>
@@ -17,13 +18,23 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <Link href="/plans" className={linkClasses}>
-            Plans
+          <Link href="/assessment" className={linkClasses}>
+            Dosha Assessment
+          </Link>
+        </li>
+        <li>
+          <Link href="/diet" className={linkClasses}>
+            Weekly Diet Plan
+          </Link>
+        </li>
+        <li>
+          <Link href="/remedies" className={linkClasses}>
+            Remedies
           </Link>
         </li>
         <li>
           <Link href="/about" className={linkClasses}>
-            About
+            About Us
           </Link>
         </li>
         <li>
